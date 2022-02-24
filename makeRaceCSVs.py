b68dfb5ac5dba8b782dedc4346c731b753872444
jenkins1085 <jenkins.1085@osu.edu>
2022-02-23 19:19:43 -0500

    Call raceResults method from getRaceResults.py for rounds 1-7, excluding the triple crown at Glendale

---
+++ makeRaceCSVs.py
@@ -0,0 +1,9 @@
+import getRaceResults
+
+getRaceResults.raceResults('https://www.supercrosslive.com/results/current/2022/S2205/S1F1RES.html', 'r1_anaheim1.csv')
+getRaceResults.raceResults('https://www.supercrosslive.com/results/current/2022/S2210/S1F1RES.html', 'r2_oakland.csv')
+getRaceResults.raceResults('https://www.supercrosslive.com/results/current/2022/S2215/S1F1RES.html', 'r3_san diego.csv')
+getRaceResults.raceResults('https://www.supercrosslive.com/results/current/2022/S2220/S1F1RES.html', 'r4_anaheim2.csv')
+# getRaceResults.raceResults('https://www.supercrosslive.com/results/current/2022/S2225/S1F1RES.html', 'r5_glendale.csv')
+getRaceResults.raceResults('https://www.supercrosslive.com/results/current/2022/S2230/S1F1RES.html', 'r6_anaheim3.csv')
+getRaceResults.raceResults('https://www.supercrosslive.com/results/current/2022/S2235/S1F1RES.html', 'r7_minneapolis.csv')
